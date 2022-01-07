eccacae49cbfc662239263a424604d49a8a6e838
Shirinzoy <rahim.shirinzoy@gmail.com>
2022-01-06 20:52:11 -0500

    First Commit

---
+++ GiTHubRepo/src/javaBasics/VariablesandAssignment_01.java
@@ -0,0 +1,20 @@
+package javaBasics;
+
+public class VariablesandAssignment_01 {
+
+	public static void main(String[] args) {
+
+	int x = 10;
+	int y = 10;
+	
+	String nameOfThisVariablesRealyLong = "sample text";
+	
+	System.out.println(nameOfThisVariablesRealyLong);
+	
+	System.out.println(x + y);
+	}
+
+}
+
+
+
